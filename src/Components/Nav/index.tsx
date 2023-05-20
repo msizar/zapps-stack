6373254ea8dfe35467fd4d884448bda69e382598
Themba <themba.msiza@relatient.net>
2023-05-20 18:51:04 +0200

    init stakerz project

---
+++ src/Components/Nav/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav: React.FC = () => {
+  return (
+    <nav className="sticky top-0 bg-gray-900 text-white py-4 px-8">
+      <h1 className="flex text-4xl font-bold">
+        <span className="App-logo pr-2">🤖</span>
+        Stackerz
+      </h1>
+
+      {/* Add your navigation links or other components here */}
+    </nav>
+  );
+};
+
+export default Nav;
