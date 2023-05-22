87a6273a802707be84e2bafa325ef1234d42d8b2
Themba <themba.msiza@relatient.net>
2023-05-22 14:50:04 +0200

    Add text

--- src/Layouts/Homepage/index.tsx
+++ src/Layouts/Homepage/index.tsx
@@ -6,6 +6,9 @@ const HomePage = () => {
     <div className="flex flex-col h-screen">
       <Nav />
       <div className="flex-grow p-4">
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to the home of Stackerz
+        </h1>
         <UsersList />
       </div>
     </div>
